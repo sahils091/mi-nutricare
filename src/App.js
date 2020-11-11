4d920d9ab6898dc20694c340c0838461ea6374f0
Sahil Singh <sahils091@gmail.com>
2020-11-11 18:53:40 -0500

    Created and styled the products component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Main from './components/Main/Main';
-import Header from './components/Navbar/Header';
-import Services from './components/Services/Services';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Main from "./components/Main/Main";
+import Header from "./components/Navbar/Header";
+import Products from "./components/Products/Products";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-     <Header/>
-     <Switch>
-      <Route path="/" exact component={Main}/>
-     <Route path="/services" component={Services}/>
-     </Switch>
-
-    </div>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,123 @@
+import React from "react";
+import "./products.scss";
+
+const Products = () => {
+  return (
+    <section className="products">
+      <div className="products__wrapper">
+        <div className="products__image">
+          <div>
+            <h1 className="products__image-title">Our Products</h1>
+          </div>
+        </div>
+        <div className="products__list">
+          <h2 className="products__list-title">Contact Us for the Specifications and Pricing</h2>
+          <h3 className="products__item">
+            BETACAROTENE 30%
+            <h4>(BASF)</h4>
+          </h3>
+          <h3 className="products__item">
+            LYCOPENE 10% DC
+            <h4>(BASF)</h4>
+          </h3>
+          <h3 className="products__item">
+            TITANIUM DIOXIDE
+            <h4>(KRONOS)</h4>
+          </h3>
+          <h3 className="products__item">
+            CROSCARMELLOSE SODIUM
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            SODIUM STARCH GLYCOLATE
+            <h4>IP /BP/USP/EP( TYPE –A/B) -POTATO /CORN BASED </h4>
+          </h3>
+          <h3 className="products__item">
+            SODIUM CARBOXYMETHYL CELLULOSE
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            GUAR GUM
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            SODIUM ALGINATE
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            CARMELLOSE CALCIUM
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            ALGINIC ACID
+            <h4>(IP/BP)</h4>
+          </h3>
+          <h3 className="products__item">
+            SODIUM AGLINATE
+            <h4>(IP/BP)</h4>
+          </h3>
+          <h3 className="products__item">
+            CALCIUM AGLINATE
+            <h4>(IP/BP)</h4>
+          </h3>
+          <h3 className="products__item">
+            SUGAR SPHERES
+            <h4>(BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            MANNITOL EXCEPIENT GRADE
+            <h4>(IP/BP/USP)</h4>
+          </h3>
+          <h3 className="products__item">
+            MANNITOL SPRAY DRIED GRADE
+            <h4>(IP/BP/USP)</h4>
+          </h3>
+          <h3 className="products__item">
+            MCC SPHERES USP
+            <h4>(NF/PH EUR)</h4>
+          </h3>
+          <h3 className="products__item">PHARMA GRADE SUCROSE.
+          </h3>
+          <h3 className="products__item">
+            DICALCIUM PHOSPHATE
+            <h4>(DI BASIC /TRI BASIC/ ANHYDROUS) IP/BP/USP/EP</h4>
+          </h3>{" "}
+          <h3 className="products__item">
+            CALCUM CARBONATE
+            <h4>(IP/BP/USP)</h4>
+          </h3>{" "}
+          <h3 className="products__item">
+            PURIFIED TALC
+            <h4>(IP/BP)</h4>
+          </h3>{" "}
+          <h3 className="products__item">
+            PREGELATINISE STARCH
+            <h4>( IP/BP/USP)</h4>
+          </h3>{" "}
+          <h3 className="products__item">
+            GLYCERINE USP
+            <h4>(P & G)</h4>
+          </h3>
+          <h3 className="products__item">HERBAL EXTRACTS
+          <h4></h4></h3>
+          <h3 className="products__item">
+            5-Amino-O-Cresol
+            <h4>(2835-95-2)</h4>
+          </h3>
+          <h3 className="products__item">
+            2,6-bis(Hydroxyethylamino)toluene sulfate
+            <h4> (144930-25-6)</h4>
+          </h3>
+          <h3 className="products__item">
+            5-nitro-O-Toludine
+            <h4>(99-55-8)</h4>
+          </h3>
+          <h3 className="products__item">Lactitol monohydrate</h3>
+          <h3 className="products__item">Triethyl citrate</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Products;
