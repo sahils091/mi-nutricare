36923097991be995e88fe7af76d0ef861454af51
Sahil Singh <sahils091@gmail.com>
2020-11-11 21:45:36 -0500

    Slight changes to header

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./navbar.scss";
+import "./header.scss";
 import Logo from "../../assets/logo.jpg";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
