9c70b9410b1d25e3b7dfacb1939ed3bfa80835e6
Sahil Singh <sahils091@gmail.com>
2020-11-18 11:22:09 -0500

    Arranged the products alphabetically

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,58 +11,60 @@ const Products = () => {
           </div>
         </div>
         <div className="products__list">
-          <h2 className="products__list-title">Contact Us for the Specifications and Pricing</h2>
-          <h3 className="products__item">
-            BETACAROTENE 30%
-            <h4>(BASF)</h4>
-          </h3>
-          <h3 className="products__item">
-            LYCOPENE 10% DC
-            <h4>(BASF)</h4>
-          </h3>
-          <h3 className="products__item">
-            TITANIUM DIOXIDE
-            <h4>(KRONOS)</h4>
-          </h3>
-          <h3 className="products__item">
-            CROSCARMELLOSE SODIUM
-            <h4>(IP/BP/USP/EP)</h4>
-          </h3>
-          <h3 className="products__item">
-            SODIUM STARCH GLYCOLATE
-            <h4>IP /BP/USP/EP( TYPE –A/B) -POTATO /CORN BASED </h4>
-          </h3>
-          <h3 className="products__item">
-            SODIUM CARBOXYMETHYL CELLULOSE
-            <h4>(IP/BP/USP/EP)</h4>
-          </h3>
-          <h3 className="products__item">
-            GUAR GUM
-            <h4>(IP/BP/USP/EP)</h4>
-          </h3>
-          <h3 className="products__item">
-            SODIUM ALGINATE
-            <h4>(IP/BP/USP/EP)</h4>
-          </h3>
-          <h3 className="products__item">
-            CARMELLOSE CALCIUM
-            <h4>(IP/BP/USP/EP)</h4>
-          </h3>
+          <h2 className="products__list-title">
+            Contact Us for the Specifications and Pricing
+          </h2>
           <h3 className="products__item">
             ALGINIC ACID
             <h4>(IP/BP)</h4>
           </h3>
           <h3 className="products__item">
-            SODIUM AGLINATE
-            <h4>(IP/BP)</h4>
+            BETACAROTENE 30%
+            <h4>(BASF)</h4>
           </h3>
           <h3 className="products__item">
             CALCIUM AGLINATE
             <h4>(IP/BP)</h4>
           </h3>
           <h3 className="products__item">
-            SUGAR SPHERES
-            <h4>(BP/USP/EP)</h4>
+            CALCUM CARBONATE
+            <h4>(IP/BP/USP)</h4>
+          </h3>
+          <h3 className="products__item">
+            CARBOXYMETHYL CELLULOSE CALCIUM
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            CARMELLOSE CALCIUM
+            <h4>( IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            CROSCARMELLOSE SODIUM
+            <h4>(IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            DICALCIUM PHOSPHATE
+            <h4>(DI BASIC /TRI BASIC/ ANHYDROUS ) IP/BP/USP/EP </h4>
+          </h3>
+          <h3 className="products__item">
+            GLYCERINE USP
+            <h4>( IP/BP/USP/EP)</h4>
+          </h3>
+          <h3 className="products__item">
+            SODIUM STARCH GLYCOLATE
+            <h4></h4>
+          </h3>
+          <h3 className="products__item">
+            HERBAL EXTRACTS
+            {/* <h4>(IP/BP)</h4> */}
+          </h3>
+          <h3 className="products__item">
+            Lactitol monohydrate
+            {/* <h4>(IP/BP)</h4> */}
+          </h3>
+          <h3 className="products__item">
+            LYCOPENE 10% DC
+            <h4>(BASF)</h4>
           </h3>
           <h3 className="products__item">
             MANNITOL EXCEPIENT GRADE
@@ -76,14 +78,13 @@ const Products = () => {
             MCC SPHERES USP
             <h4>(NF/PH EUR)</h4>
           </h3>
-          <h3 className="products__item">PHARMA GRADE SUCROSE.
-          </h3>
+          <h3 className="products__item">PHARMA GRADE SUCROSE.</h3>
           <h3 className="products__item">
-            DICALCIUM PHOSPHATE
-            <h4>(DI BASIC /TRI BASIC/ ANHYDROUS) IP/BP/USP/EP</h4>
+            PHARMA GRADE SUCROSE
+            {/* <h4>(DI BASIC /TRI BASIC/ ANHYDROUS) IP/BP/USP/EP</h4> */}
           </h3>{" "}
           <h3 className="products__item">
-            CALCUM CARBONATE
+            PREGELATINISE STARCH
             <h4>(IP/BP/USP)</h4>
           </h3>{" "}
           <h3 className="products__item">
@@ -91,15 +92,22 @@ const Products = () => {
             <h4>(IP/BP)</h4>
           </h3>{" "}
           <h3 className="products__item">
-            PREGELATINISE STARCH
-            <h4>( IP/BP/USP)</h4>
+            SODIUM AGLINATE
+            <h4> (IP/BP/USP/EP)</h4>
           </h3>{" "}
           <h3 className="products__item">
-            GLYCERINE USP
-            <h4>(P & G)</h4>
+            SODIUM CARBOXYMETHYL CELLULOSE
+            <h4>IP /BP/USP/EP( TYPE –A/B) - POTATO /CORN BASED</h4>
           </h3>
-          <h3 className="products__item">HERBAL EXTRACTS
+          <h3 className="products__item">
+            SUGAR SPHERES
+            <h4> ( BP/USP/EP)</h4>
           </h3>
+          <h3 className="products__item">
+            TITANIUM DIOXIDE
+            <h4> (KRONOS)</h4>
+          </h3>
+          <h3 className="products__item">Triethyl citrate</h3>
           <h3 className="products__item">
             5-Amino-O-Cresol
             <h4>(2835-95-2)</h4>
@@ -112,8 +120,6 @@ const Products = () => {
             5-nitro-O-Toludine
             <h4>(99-55-8)</h4>
           </h3>
-          <h3 className="products__item">Lactitol monohydrate</h3>
-          <h3 className="products__item">Triethyl citrate</h3>
         </div>
       </div>
     </section>
